1fd07a345721d5db8cf2de408b1105aad95927e7
PC3961 <cpranavss65@gmail.com>
2023-11-13 11:40:20 +0530

    backend

--- schemas/index.js
+++ schemas/index.js
@@ -2,4 +2,5 @@ import testimonials from "./testimonials"
 import abouts from "./abouts"
 import brands from "./brands"
 import contacts from "./contacts"
-export const schemaTypes = [testimonials, abouts, brands, contacts]
+import skills from "./skills"
+export const schemaTypes = [testimonials, abouts, brands, contacts, skills]
